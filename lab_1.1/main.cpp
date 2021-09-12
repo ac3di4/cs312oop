9007cc469fecb8bf8b3b29e5da837c833592e19d
Sorochan Ilya <fullof.acedia@gmail.com>
2021-09-12 11:57:24 +0300

    Dialog program

--- lab_1.1/main.cpp
+++ lab_1.1/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <iomanip>
 
+#include "Deltoid.hpp"
+
+using curves::Deltoid;
+
+/// Example usage:
+/// in: 3
+/// out: 4 16 ~6
+/// in: 0
+/// out: (3, 0)
 int main() {
-    std::cout << "Hello world" << std::endl;
+    // dialog program
+    std::cout << "Deltoid bigger circle radius R=3r=";
+    double r;
+    std::cin >> r;
+    if (!std::cin.good())
+        return -1;
+
+    Deltoid d(r);
+    std::cout << std::setprecision(2)
+              << "Intersection with tangent length: " << d.intersect_len() << std::endl
+              << "Curve length: " << d.len() << std::endl
+              << "Curve area: " << d.area() << std::endl
+              << "Cartesian equation: " << d.equation() << std::endl;
+
+    std::cout << "Enter t parameter: ";
+    double t;
+    std::cin >> t;
+    if (!std::cin.good())
+        return -1;
+
+    auto pos = d.xy(t);
+    std::cout << "Deltoid curve coordinates for t=" << t
+              << "(" << pos.first << ", " << pos.second << ")" << std::endl;
+
+    return 0;
 }
